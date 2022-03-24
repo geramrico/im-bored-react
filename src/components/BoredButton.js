c43b7e20fc77719f84485bf35b84d6b2b8edbf11
Gerardo Morales <geramoralesrico+code@gmail.com>
2022-03-23 22:28:20 -0600

    changed http for https

--- src/components/BoredButton.js
+++ src/components/BoredButton.js
@@ -10,8 +10,8 @@ const BoredButton = () => {
   const typeOptions = ['education', 'recreational', 'social', 'diy', 'charity', 'cooking', 'relaxation', 'music', 'busywork']
 
   const getActivity = async () => {
-    const response = await fetch(`http://www.boredapi.com/api/activity`)
-    // const response = await fetch(`http://www.boredapi.com/api/activity?type=${activityType}`)
+    // const response = await fetch(`https://www.boredapi.com/api/activity`)
+    const response = await fetch(`https://www.boredapi.com/api/activity?type=${activityType}`)
     const data = await response.json()
     setBtnText('Show me more')
     setActivity(data.activity)
